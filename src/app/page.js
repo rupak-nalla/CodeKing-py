e16d0cfe79900b7471f1ccd57b599a87c5a21966
Nalla_Rupak <rupaknalla1034@gmail.com>
2024-06-11 02:29:42 +0530

    updated css

--- src/app/page.js
+++ src/app/page.js
@@ -274,7 +274,7 @@ export default function Home() {
           
         </div>
             </nav>
-            <div class="flex flex-col md:flex-row h-screen">
+            <div class="flex flex-col md:flex-row min-h-screen">
               <div class="w-full md:w-3/4 bg-gray-200 p-8">
                 {/* <!-- Content for the first column --> */}
                 <div class="min-h-screen flex flex-col" id="qp">
